326e8581e19efddccd83c5490d2520cece61072e
Asma <asmabrazi27@gmail.com>
2020-12-18 18:25:36 +0100

    rename html file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__,template_folder='.')
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+   return render_template('home.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
