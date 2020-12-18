f9fee927a158866cd6ade2e77b798bbc7954d9be
Asma <asmabrazi27@gmail.com>
2020-12-18 19:59:47 +0100

    photoRecognize

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app = Flask(__name__,template_folder='templates')
 def index():
    return render_template('index.html')
 
+
+@app.route('/photoRecognize', methods=['POST'])
+def photoRecognize():
+    if request.method == 'POST': 
+        data = request.files['image_data']
+        if data == None:
+            return 'no image received'
+        else:
+            # model.predict.predict returns a dictionary
+            prediction = predict(data)
+    else:
+      return render_template('index.html')
+
+    return jsonify(status='OK', results=prediction)
+
+    
 if __name__ == '__main__':
    app.run(debug = True)
