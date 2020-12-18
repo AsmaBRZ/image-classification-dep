9bdfcfef74bbc64c013b75c7064099f21f4535d4
Asma <asmabrazi27@gmail.com>
2020-12-18 17:56:20 +0100

    helloworld

--- app.py
+++ app.py
@@ -1,30 +1,10 @@
-from flask import Flask,request, render_template
-#from model.prediction import *
-#from flask import jsonify 
+from flask import Flask
 
-# create the flask object
-
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return render_template('index.html')
-
-"""
-@app.route('/photoRecognize', methods=['POST'])
-def photoRecognize():
-    if request.method == 'POST': 
-        data = request.files['image_data']
-        if data == None:
-            return 'no image received'
-        else:
-            # model.predict.predict returns a dictionary
-            prediction = predict(data)
-    else:
-      return render_template('index.html')
-
-    return jsonify(status='OK', results=prediction)
-"""
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def index():
+   return '<html><body><h1>Hello World</h1></body></html>'
+   
+if __name__ == '__main__':
+   app.run(debug = True)
