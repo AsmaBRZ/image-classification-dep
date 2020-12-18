bd75ce25eb176d0f096a82d6713338256f145f3e
Asma <asmabrazi27@gmail.com>
2020-12-18 20:17:28 +0100

    saved_model/my_model

--- model/prediction.py
+++ model/prediction.py
@@ -13,7 +13,7 @@ def predict(data):
     global model_w
 
     if model_w is None:
-        model_w = tf.keras.models.load_model('model')
+        model_w = tf.keras.models.load_model('saved_model/my_model')
         model_w.load_weights('my_model.h5') 
 
     CIFAR10_CLASSES = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
